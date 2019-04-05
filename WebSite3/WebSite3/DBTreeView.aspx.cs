92e6750bd80648fa0649bfd1c6721fb22ad1959f
60023180 <32890847+60023180@users.noreply.github.com>
2019-04-05 18:24:11 +0700

    DB

---
+++ WebSite3/WebSite3/DBTreeView.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class DBTreeView : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            string StrConn = WebConfigurationManager.ConnectionStrings
+                ["UPPart2ConnectionString"].ConnectionString;
+            using (SqlConnection ObjConn = new SqlConnection(StrConn))
+            {
+                ObjConn.Open();
+                using (SqlCommand ObjCM = new SqlCommand())
+                {
+                    ObjCM.Connection = ObjConn;
+                    ObjCM.CommandText = "SELECT ID, name, part FROM TreeView WHERE part IS NULL";
+                    SqlDataReader ObjDR = ObjCM.ExecuteReader();
+                    while (ObjDR.Read())
+                    {
+                        TreeNode node = new TreeNode(ObjDR["name"].ToString(), ObjDR["id"].ToString());
+                        TreeView1.Nodes.Add(node);
+                        TreeViewNodes(ObjDR["id"].ToString(), node);
+                    }
+                    ObjDR.Close();
+                }
+                ObjConn.Close();
+            }
+        }
+    }
+    private void TreeViewNodes(string part, TreeNode partNode)
+    {
+        string StrConn = WebConfigurationManager.ConnectionStrings
+                ["UPPart2ConnectionString"].ConnectionString;
+        using (SqlConnection ObjConn = new SqlConnection(StrConn))
+        {
+            ObjConn.Open();
+            using (SqlCommand ObjCM = new SqlCommand())
+            {
+                ObjCM.Connection = ObjConn;
+                ObjCM.CommandText = "SELECT ID, name, part FROM TreeView WHERE Part =" + part;
+                SqlDataReader ObjDR = ObjCM.ExecuteReader();
+                while (ObjDR.Read())
+                {
+                    TreeNode node = new TreeNode(ObjDR["name"].ToString(), ObjDR["id"].ToString());
+                    partNode.ChildNodes.Add(node);
+                }
+                ObjDR.Close();
+            }
+            ObjConn.Close();
+        }
+    }
+}
---
+++ WebSite3/WebSite3/ThreeView.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="ThreeView.aspx.cs" Inherits="ThreeView" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:TreeView ID="TreeView1" runat="server" ShowLines="True">
+    <Nodes>
+        <asp:TreeNode Text="New Node" Value="New Node">
+            <asp:TreeNode Text="New Node" Value="New Node"></asp:TreeNode>
+        </asp:TreeNode>
+        <asp:TreeNode Text="New Node" Value="New Node"></asp:TreeNode>
+        <asp:TreeNode Text="New Node" Value="New Node"></asp:TreeNode>
+    </Nodes>
+</asp:TreeView>
+</asp:Content>
+
